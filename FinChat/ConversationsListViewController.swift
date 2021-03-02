cff82ebf10746058ecfdc1bf8ac4c713e520067a
Artem <artmurashko@yandex.ru>
2021-03-02 12:07:44 +0300

    Complete ConversationsListScreen

--- FinChat/ConversationsListViewController.swift
+++ FinChat/ConversationsListViewController.swift
@@ -8,14 +8,80 @@
 import UIKit
 
 class ConversationsListViewController: UIViewController {
-    @IBOutlet weak var buttonProfile: UIBarButtonItem!
     
+    @IBOutlet weak var buttonProfile: UIBarButtonItem!
+    @IBOutlet weak var tableViewMessages: UITableView!
+    
+    // Temporary structure CellModel and array listOfUsers for test
+    struct CellModel {
+        var name: String?
+        var message: String?
+        var date: Date?
+        var online: Bool
+        var hasUnreadMessages: Bool
+    }
+    
+    let listOfUsers : [CellModel] = [CellModel(name: "Artem Murashko", message: "Where are you?", date: Date.init(),                                      online: true, hasUnreadMessages: true),
+                                    CellModel(name: "Barack Obama", message: "What's up bro!", date: Date.init(timeIntervalSinceNow: -240), online: true, hasUnreadMessages: false),
+                                    CellModel(name: "Laylah Slater", message: "Okay!", date: nil, online: true, hasUnreadMessages: true),
+                                    CellModel(name: "Ella Fulton", message: "Hahaha :D", date: nil, online: true, hasUnreadMessages: false),
+                                    CellModel(name: "Ayaana Church", message: nil, date: nil, online: true, hasUnreadMessages: false),
+                                    CellModel(name: "Mikolaj Mcconnell", message: nil, date: nil, online: true, hasUnreadMessages: true),
+                                    CellModel(name: nil, message: nil, date: nil, online: true, hasUnreadMessages: false),
+                                    CellModel(name: nil, message: nil, date: nil, online: true, hasUnreadMessages: true),
+                                    CellModel(name: nil, message: "Idk dude", date: Date.init(timeIntervalSinceNow: -86400), online: true, hasUnreadMessages: true),
+                                    CellModel(name: "Humzah Mckay", message: nil, date: Date.init(timeIntervalSinceNow: -259200), online: true, hasUnreadMessages: false),
+                                    CellModel(name: "Nafeesa Preece", message: "Who r u?", date: Date.init(timeIntervalSinceNow: -600),  online: false, hasUnreadMessages: true),
+                                    CellModel(name: "Lindsay Bond", message: "Mmm. Sound's good!", date: Date.init(timeIntervalSinceNow: -1200), online: false, hasUnreadMessages: false),
+                                    CellModel(name: "Nakita Raymond", message: "What was in that picture?", date: nil, online: false, hasUnreadMessages: true),
+                                    CellModel(name: "Vihaan Gibbs", message: "Rly? I can't believe it! :D", date: nil, online: false, hasUnreadMessages: false),
+                                    CellModel(name: "Amin Daniel", message: nil, date: nil, online: false, hasUnreadMessages: false),
+                                    CellModel(name: "Menna Frazier", message: nil, date: nil, online: false, hasUnreadMessages: true),
+                                    CellModel(name: nil, message: nil, date: nil, online: false, hasUnreadMessages: false),
+                                    CellModel(name: nil, message: nil, date: nil, online: false, hasUnreadMessages: true),
+                                    CellModel(name: nil, message: "How much is it?", date: Date.init(timeIntervalSinceNow: -86400), online: false, hasUnreadMessages: true),
+                                    CellModel(name: "Yes, it's me", message: nil, date: Date.init(timeIntervalSinceNow: -259200), online: false, hasUnreadMessages: false),
+    ]
+    
+    let headers : [String] = ["Online", "History"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "Tinkoff Chat"
-        
+        // Creating a table
+        tableViewMessages.register(CustomConversationListTableViewCell.nib(), forCellReuseIdentifier: CustomConversationListTableViewCell.identifier)
+        tableViewMessages.delegate = self
+        tableViewMessages.dataSource = self
+    }
+}
+
+extension ConversationsListViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return headers.count
     }
 
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headers[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // If section == 0, then return number of online users and otherwise
+        return listOfUsers.filter{$0.online == (section == 0 ? true : false) }.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomConversationListTableViewCell.identifier, for: indexPath) as? CustomConversationListTableViewCell else { return UITableViewCell() }
+        
+        
+        let cellModel = listOfUsers.filter{$0.online == (indexPath.section == 0 ? true : false)}[indexPath.row]
+        
+        cell.configure(with: .init(name: cellModel.name, message: cellModel.message, date: cellModel.date, online: cellModel.online, hasUnreadMessages: cellModel.hasUnreadMessages))
+    
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
 }
---
+++ FinChat/CustomConversationListTableViewCell.swift
@@ -0,0 +1,75 @@
+//
+//  CustomConversationTableViewCell.swift
+//  FinChat
+//
+//  Created by Артём Мурашко on 01.03.2021.
+//
+
+import UIKit
+
+protocol ConversationCellConfiguration {
+    var name : String? {get set}
+    var message : String? {get set}
+    var date : Date? {get set}
+    var online : Bool {get set}
+    var hasUnreadMessages : Bool {get set}
+}
+
+class CustomConversationListTableViewCell: UITableViewCell {
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelMessage: UILabel!
+    @IBOutlet weak var labelDate: UILabel!
+    
+    static let identifier = "CustomConversationTableViewCell"
+    static func nib() -> UINib {
+        return UINib(nibName: "CustomConversationTableViewCell", bundle: nil)
+    }
+    
+    struct Model : ConversationCellConfiguration {
+        var name: String?
+        var message: String?
+        var date: Date?
+        var online: Bool
+        var hasUnreadMessages: Bool
+    }
+    
+    func configure(with pattern: Model) {
+        if let name = pattern.name {
+            self.labelName.text = name
+        } else {
+            self.labelName.text = "Unknown"
+        }
+        
+        if let message = pattern.message {
+            self.labelMessage.text = message
+        } else {
+            self.labelMessage.text = "No messages yet"
+            self.labelMessage.font = UIFont(name:"HelveticaNeue-Italic", size: 17.0)
+            
+        }
+        
+        if let date = pattern.date {
+            let calendar = NSCalendar.autoupdatingCurrent
+            let formatter = DateFormatter()
+            
+            formatter.dateFormat = calendar.isDateInToday(date) ? "HH:mm" : "dd MMM"
+            
+            self.labelDate.text = formatter.string(from: date)
+        } else {
+            self.labelDate.text = ""
+        }
+        
+        if pattern.online == true {
+            self.contentView.backgroundColor = UIColor(red: 251.0/255.0, green: 242.0/255.0, blue: 212.0/255.0, alpha: 1.0)
+        } else {
+            self.contentView.backgroundColor = .white
+        }
+        
+        if pattern.hasUnreadMessages == true {
+            self.labelMessage.font = .boldSystemFont(ofSize: 17.0)
+        } else {
+            self.labelMessage.font = .systemFont(ofSize: 17.0)
+        }
+    }
+}
+
