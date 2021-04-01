cc6cda79ff3a927369ed2c3e5d467e97e1e454ab
Artem <artmurashko@yandex.ru>
2021-04-01 22:57:20 +0300

    Added folders and Data Model

---
+++ FinChat/CoreData/CoreDataStack.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStack.swift
+//  FinChat
+//
+//  Created by Артём Мурашко on 01.04.2021.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    var didUpdateDataBase: ((CoreDataStack) -> Void)?
+    
+}
